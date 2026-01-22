241cd592ab68b505409641d897e34a160d1253af
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-22 06:24:36 +0530

    624

---
+++ testt.py
@@ -0,0 +1,11 @@
+#WAP
+for i in range(6):
+    for j in range(6):
+        print("*", end="")
+    print()   
+
+
+for i in range(1,6):
+    for j in range(i,6):
+        print("*", end="")
+    print()         
