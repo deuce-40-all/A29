7d84f4c5d5d79d4b62d34969d7e9e306b5ae8a3f
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-27 22:47:14 +0530

    5645

--- 118.py
+++ 118.py
@@ -1,8 +1,8 @@
-#WAP 
+#WAP to 
 
 a = 'Python is very easy'.split()
 b={}
-for i in range(1,len(a)):
+for i in range(0,len(a)):
     if i%2==0:
         for j in a[i]:
             b[i]=a[i][::-1]+str(len(a[i]))
